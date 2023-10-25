cb926999aca796aad46a713b8ab684a1b12a4204
daniel <garcia.daniel609025@gmail.com>
2023-10-25 11:21:54 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import JokeList from "./JokeList";
+
+
+/** Top-level component. */
+
+function App() {
+  return (
+    <div className="App">
+      <JokeList />
+    </div>
+  );
+}
+
+export default App;
